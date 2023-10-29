ea66c3a4a52b6b8095edecdea307f2045690b52d
EvitanRelta <shauntanzongzhi@gmail.com>
2023-10-29 12:07:06 +0800

    1st draft

--- Alice.py
+++ Alice.py
@@ -1,1 +1,61 @@
-# blank
+import socket
+import sys
+
+from data_classes import Header, Segment
+
+HOST_IP = "127.0.0.1"
+MAX_PAYLOAD_BYTES = 64  # Inclusive of headers
+TIMEOUT_MS = 50
+MAX_INPUT_BYTES = 5000
+
+
+def main() -> None:
+    unreli_net_address = (HOST_IP, get_unreli_net_port())
+    segments = segment_data(read_data())
+
+    # Connect to UnreliNET via UDP.
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.bind(unreli_net_address)
+        s.settimeout(TIMEOUT_MS / 1000)
+
+        for segment in segments:
+            sequence_num = segment.header.sequence_num
+
+            while True:
+                s.sendto(segment.encode(), unreli_net_address)
+                try:
+                    data, _ = s.recvfrom(MAX_PAYLOAD_BYTES)
+                    ack = Segment.decode(data)
+                    if ack.has_no_bit_error() and ack.header.ack_num == sequence_num:
+                        break
+                except socket.timeout:
+                    pass
+
+
+def get_unreli_net_port() -> int:
+    return int(sys.argv[1])
+
+
+def read_data() -> bytes:
+    return sys.stdin.buffer.read(MAX_INPUT_BYTES)
+
+
+def segment_data(data: bytes) -> list[Segment]:
+    chunk_size = MAX_PAYLOAD_BYTES - Header.HEADER_SIZE_BYTES
+
+    # Split data into chunks.
+    data_chunks: list[bytes] = []
+    for i in range(0, len(data), chunk_size):
+        data_chunks.append(data[i : i + chunk_size])
+
+    return [
+        Segment.create_data_segment(
+            sequence_num=i % 2,
+            data=data,
+        )
+        for i, data in enumerate(data_chunks)
+    ]
+
+
+if __name__ == "main":
+    main()
--- Bob.py
+++ Bob.py
@@ -1,1 +1,40 @@
-# blank
+import socket
+import sys
+from typing import Literal
+
+from data_classes import Segment
+
+HOST_IP = "127.0.0.1"
+MAX_PAYLOAD_BYTES = 64  # Inclusive of headers
+TIMEOUT_MS = 50
+MAX_INPUT_BYTES = 5000
+
+
+def main() -> None:
+    unreli_net_address = (HOST_IP, get_unreli_net_port())
+    latest_received_sequence_num: Literal[0, 1] = 0
+
+    # Connect to UnreliNET via UDP.
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.bind(unreli_net_address)
+
+        data, _ = s.recvfrom(MAX_PAYLOAD_BYTES)
+        segment = Segment.decode(data)
+
+        if (
+            segment.has_no_bit_error()
+            and segment.header.sequence_num != latest_received_sequence_num
+        ):
+            print(segment.data.decode("ascii"))
+            latest_received_sequence_num = segment.header.sequence_num  # type: ignore
+
+        ack = Segment.create_ack(ack_num=segment.header.sequence_num)
+        s.sendto(ack.encode(), unreli_net_address)
+
+
+def get_unreli_net_port() -> int:
+    return int(sys.argv[1])
+
+
+if __name__ == "main":
+    main()
